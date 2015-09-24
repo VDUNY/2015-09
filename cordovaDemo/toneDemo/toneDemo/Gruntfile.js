bbf34a41fabc6a6222e484bdc0d9788f531e6cea
Matt Frey <matthew.t.frey@gmail.com>
2015-09-23 21:17:23 -0400

    init

---
+++ cordovaDemo/toneDemo/toneDemo/Gruntfile.js
@@ -0,0 +1,20 @@
+﻿/// <binding ProjectOpened='watch:javascript' />
+module.exports = function(grunt) {
+    grunt.initConfig({
+        browserify: {
+            'www/scripts/browserifyd.js': ['www/scripts/index.js']
+        },
+        watch: {
+            javascript: {
+                files: 'www/scripts/index.js',
+                tasks: ['browserify']
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    
+};
